f22b092e606f1c5b11e9ed240431566b957be91c
xct <xcat182@gmail.com>
2021-03-11 13:48:41 +0300

    feat: implement max function

--- src/index.js
+++ src/index.js
@@ -2,16 +2,22 @@
 exports.min = function min (array) {
   
   let arr = Array.of(array).flat(); 
+  if (arr.length == 0 || array === undefined) {
+    return 0;
+  }
+  return Math.min( ...arr );
+  
+}
+
+exports.max = function max (array) {
+  
+  let arr = Array.of(array).flat(); 
 
   if (arr.length == 0 || array === undefined) {
     return 0;
   }
 
-  return Math.min( ...arr );
-}
-
-exports.max = function max (array) {
-  return 0;
+  return Math.max( ...arr );
 }
 
 exports.avg = function avg (array) {
